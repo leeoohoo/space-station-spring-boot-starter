18be3a70515de1447fe8f750d4fc1fc70ae3a22b
lli30 <1798644410@qq.com>
2022-11-25 12:12:28 +0800

    dtoquery

--- src/main/java/com/oohoo/spacestationspringbootstarter/dto/query/TestDto.java
+++ src/main/java/com/oohoo/spacestationspringbootstarter/dto/query/TestDto.java
@@ -2,15 +2,24 @@ package com.oohoo.spacestationspringbootstarter.dto.query;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 /**
  * @author Lei Li. lei.d.li@capgemini.com
  * @Description
  * @since 23 November 2022
  */
 @Data
+@Entity
 public class TestDto implements DTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
     private Long dtoId;
 
     private String dtoName;
+
+
 }
